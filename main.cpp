cb4aa6100c26661b25a2c60f34c95d95485a768f
Евгений Колесенко <evgeniy.kolesenko@gmail.com>
2025-10-04 14:18:00 +0300

    console version

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// console version
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    system("chcp 65001 > nul");
+    ofstream MyFile("mynote.txt");
+
+    string myText;
+    getline(cin, myText);
+
+    MyFile << myText;
+    MyFile.close();
+    
+
+    ifstream ReadFile("mynote.txt");
+    while (getline(ReadFile, myText))
+    {
+        cout<<myText;
+    }
+
+    ReadFile.close();
+    
+    return 0;
+}
