f7186b77dee9295adf9c371bc576dff990b7c6e2
Евгений Колесенко <evgeniy.kolesenko@gmail.com>
2025-10-04 22:18:31 +0300

    ui changes

--- test.cpp
+++ test.cpp
@@ -1,9 +1,15 @@
-
 #include <windows.h>
+
+#define FILE_MENU_NEW 1
+#define FILE_MENU_OPEN 2
+#define FILE_MENU_EXIT 3
 
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
+void AddMenus(HWND);
+
+HMENU hMenu;
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
 {
@@ -39,10 +45,45 @@ LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg)
     {
+    case WM_COMMAND:
+        switch (wp)
+        {
+            case FILE_MENU_EXIT:
+                DestroyWindow(hWnd);
+                return 0;
+            case FILE_MENU_NEW:
+                MessageBeep(MB_ICONINFORMATION);
+                return 0;
+        }
+        return 0;
+    case WM_CREATE:
+        AddMenus(hWnd);
+        return 0;
     case WM_DESTROY:
        PostQuitMessage(0);
        return 0;
     default:
         return DefWindowProcW(hWnd, msg, wp, lp);
     }
+}
+
+void AddMenus(HWND hWnd)
+{
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+    HMENU hSubMenu = CreateMenu();
+
+    AppendMenu(hSubMenu, MF_STRING, 1001, TEXT("SubMenu Item"));
+
+
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_NEW, TEXT("New"));
+    AppendMenu(hFileMenu, MF_POPUP, (UINT_PTR)hSubMenu, TEXT("Open SubMenu"));
+    AppendMenu(hFileMenu, MF_SEPARATOR, 0, NULL);
+    AppendMenu(hFileMenu, MF_STRING, FILE_MENU_EXIT, TEXT("Exit"));
+
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hFileMenu, TEXT("File"));
+    AppendMenu(hMenu, MF_STRING, 1002, TEXT("Help"));
+
+    SetMenu(hWnd, hMenu);
+
 }
